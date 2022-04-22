f1e3816f5851e2ccee238da32f8455ba38c26a87
pguyon <pguyon69@gmail.com>
2022-04-22 15:48:56 +0200

    add JS code to close  modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalContent = document.querySelector(".content");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,15 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Add function on close for display modal content and background content
+document.querySelector(".close").addEventListener("click", function () {
+  modalContent.style.display = "none";
+  modalbg.style.display = "none";
+});
 
+// check if content was display
+document.querySelector(".btn-signup").addEventListener("click", function () {
+  if ((modalContent.style.display = "none")) {
+    modalContent.style.display = "block";
+  }
+});
